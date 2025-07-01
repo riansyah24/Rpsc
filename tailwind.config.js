335349a2018997689023bb39ec9c7ada3f7b1ea5
riansyah24 <jackrian72@gmail.com>
2025-07-01 20:05:02 +0700

    fix: fix css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html","./game.html"],
+  content: ["./index.html","./game.html","./www/index.html","./www/game.html"],
   theme: {
     extend: {
       "-100vh" : "-100vh",
